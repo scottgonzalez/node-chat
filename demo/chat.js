a67ba34c21389003ac1ef3d118a0fc7a3ac7a417
Scott González <scott.gonzalez@gmail.com>
2010-03-19 22:57:54 -0400

    Pulled all files related to demo out of chat module.

--- demo/chat.js
+++ demo/chat.js
@@ -1,3 +1,10 @@
-var chat = require('../lib/server');
+var chat = require('../lib/server'),
+	router = require("../lib/router");
 
-chat.createServer().listen(8001);
+var chatServer = chat.createServer();
+chatServer.listen(8001);
+httpServer = chatServer.httpServer;
+httpServer.get("/", router.staticHandler("index.html"));
+httpServer.get("/style.css", router.staticHandler("style.css"));
+httpServer.get("/client.js", router.staticHandler("client.js"));
+httpServer.get("/jquery-1.2.6.min.js", router.staticHandler("jquery-1.2.6.min.js"));
