480aaa392c74528c59da38cc057330ba3388959c
Scott González <scott.gonzalez@gmail.com>
2010-04-11 17:24:53 -0400

    Use .forEach() instead of a for loop for image handling.

--- demo/chat.js
+++ demo/chat.js
@@ -1,9 +1,12 @@
 var chat = require('../lib/server'),
 	router = require("../lib/router");
 
+// create chat server and a single channel
 var chatServer = chat.createServer();
 chatServer.listen(8001);
 chatServer.addChannel({ basePath: "/chat" });
+
+// chat app
 chatServer.passThru("/", router.staticHandler("index.html"));
 
 // CSS
@@ -11,14 +14,13 @@ chatServer.passThru("/css/layout.css", router.staticHandler("css/layout.css"));
 chatServer.passThru("/css/reset.css", router.staticHandler("css/reset.css"));
 
 // Images
-var files = ["background.png", "button.png", "footer.png", "glows.png", "header-bg.png", "inset-border-l.png", "inset-border.png", "metal.jpg","node-chat.png" ,"send.png"];
+["background.png", "button.png", "footer.png", "glows.png", "header-bg.png",
+	"inset-border-l.png", "inset-border.png", "metal.jpg", "node-chat.png",
+	"send.png"].forEach(function(file) {
+		chatServer.passThru("/images/" + file, router.staticHandler("images/" + file));
+	});
 
-for (var i = files.length - 1; i >= 0; i--){
-  chatServer.passThru("/images/" + files[i], router.staticHandler("images/" + files[i]));  
-};
-
-//
-
+// JS
 chatServer.passThru("/jquery-1.4.2.js", router.staticHandler("jquery-1.4.2.js"));
 chatServer.passThru("/nodechat.js", router.staticHandler("nodechat.js"));
 chatServer.passThru("/client.js", router.staticHandler("client.js"));
