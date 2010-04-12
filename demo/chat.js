a8784b651d6f09171cffeae87ce9561e2bbeedfc
Scott González <scott.gonzalez@gmail.com>
2010-04-11 20:17:19 -0400

    Moved web files into their own directory.

--- demo/chat.js
+++ demo/chat.js
@@ -7,20 +7,20 @@ chatServer.listen(8001);
 chatServer.addChannel({ basePath: "/chat" });
 
 // chat app
-chatServer.passThru("/", router.staticHandler("index.html"));
+chatServer.passThru("/", router.staticHandler("web/index.html"));
 
 // CSS
-chatServer.passThru("/css/layout.css", router.staticHandler("css/layout.css"));
-chatServer.passThru("/css/reset.css", router.staticHandler("css/reset.css"));
+chatServer.passThru("/css/layout.css", router.staticHandler("web/css/layout.css"));
+chatServer.passThru("/css/reset.css", router.staticHandler("web/css/reset.css"));
 
 // Images
 ["background.png", "button.png", "footer.png", "glows.png", "header-bg.png",
 	"inset-border-l.png", "inset-border.png", "metal.jpg", "node-chat.png",
 	"send.png"].forEach(function(file) {
-		chatServer.passThru("/images/" + file, router.staticHandler("images/" + file));
+		chatServer.passThru("/images/" + file, router.staticHandler("web/images/" + file));
 	});
 
 // JS
-chatServer.passThru("/jquery-1.4.2.js", router.staticHandler("jquery-1.4.2.js"));
-chatServer.passThru("/nodechat.js", router.staticHandler("nodechat.js"));
-chatServer.passThru("/client.js", router.staticHandler("client.js"));
+chatServer.passThru("/js/jquery-1.4.2.js", router.staticHandler("web/js/jquery-1.4.2.js"));
+chatServer.passThru("/js/nodechat.js", router.staticHandler("web/js/nodechat.js"));
+chatServer.passThru("/js/client.js", router.staticHandler("web/js/client.js"));
